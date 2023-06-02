c10a326d1cc361a2774f8483c9a9ac41a9b4f616
JDuque <diegoduqqe@gmail.com>
2023-06-01 23:45:11 -0500

    organización dataframe

---
+++ app/services/dataframe_service.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+from sklearn.calibration import LabelEncoder
+from sklearn.discriminant_analysis import StandardScaler
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, confusion_matrix,precision_score, recall_score, f1_score
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
+
+
+
+class DataframeService:
+
+    def __init__(self):
+         pass
+
+    def codificar_valores_cat(self, dataframe):
+        #print(dataframe)
+        encoder=LabelEncoder()
+        cat_colsAll = [col for col in dataframe.columns if dataframe[col].dtype == 'object']
+        # Obtener columnas a encodificar
+        for col in cat_colsAll:
+                dataframe[col] = encoder.fit_transform(dataframe[col])
+        #print(dataframe)
+
+        return dataframe
+    
+    def  normalizar_informacion(self, dataframe):
+         #print("dataframe")
+         dataNumerica = dataframe.select_dtypes(np.number)
+         escalador=StandardScaler()
+         dataNumerica=pd.DataFrame(escalador.fit_transform(dataNumerica), columns = dataNumerica.columns)
+         #print("Normalizado")
+         #print(dataNumerica)
+         return dataNumerica
+    
+    def redondear_datos(self, dataNumerica):
+         #print(dataNumerica)
+         dataNumerica = dataNumerica.astype(int)
+         #print("Redondeado")
+         #print(dataNumerica)
+         return dataNumerica
+    
+    def concatenar_datos(self, dataNumerica, dataframe):
+        dataCategorica = dataframe.select_dtypes(object)
+        dataFinal = pd.concat([dataNumerica, dataCategorica], axis=1)
+        return dataFinal
+    
