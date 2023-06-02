fdc1c351b10707d5adc69d13601fc49dab88b12b
JDuque <diegoduqqe@gmail.com>
2023-06-02 16:55:52 -0500

    para guardar y acceder a otras colecciones

--- app/services/mongodb_service.py
+++ app/services/mongodb_service.py
@@ -6,10 +6,11 @@ class MongoDBService:
         mongoDB = MongoConnection()
         self.client = mongoDB.get_mongo_connection()
         self.db = self.client['MLearning']
-        self.collection = self.db['Dataset']
+        self.collection = None
 
-    def guardar_json(self, datos_json):
+    def guardar_json(self, datos_json, coleccion):
         try:
+            self.collection = self.db[coleccion]
             result = self.collection.insert_one(datos_json)
             print(f'JSON guardado en MongoDB : {str(result)}')
             return result.inserted_id
@@ -17,8 +18,9 @@ class MongoDBService:
             print(f'Error al guardar el JSON en MongoDB: {str(e)}')
             return None
         
-    def obtener_ultimo_registro(self):
+    def obtener_ultimo_registro(self, coleccion):
         try:
+            self.collection = self.db[coleccion]
             result = self.collection.find_one(sort=[('_id', -1)])
             # print(f'JSON obtenido en MongoDB\n : {str(result)}')
             # print(type(result))
