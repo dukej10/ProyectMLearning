453b9a439906db0d5fea012c220270de944b0d8c
JDuque <diegoduqqe@gmail.com>
2023-06-03 01:29:45 -0500

    kn  recibe parametros

--- app/Controllers/mlearning_controller.py
+++ app/Controllers/mlearning_controller.py
@@ -6,8 +6,9 @@ class MLearningController:
     def __init__(self):
         self.mlearning_service = MLearningService()
 
-    def knn(self):
-        return self.mlearning_service.knn()
+    def knn(self, entrenamiento):
+        return self.mlearning_service.knn(entrenamiento)
     
     def reg_logistica(self):
-        return self.mlearning_service.regresion_logistica()
+        #return self.mlearning_service.regresion_logistica()
+        return "ok"
