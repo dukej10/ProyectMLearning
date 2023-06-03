31550aecc4e98c93baed0b18a7da22236226f6d9
JDuque <diegoduqqe@gmail.com>
2023-06-03 11:50:36 -0500

    identificación de algoritmos

--- app/Controllers/mlearning_controller.py
+++ app/Controllers/mlearning_controller.py
@@ -1,3 +1,4 @@
+from app.models.entrenamiento_model import InfoEntrenamiento
 from app.services.mlearning_service import MLearningService
 
 
@@ -6,8 +7,22 @@ class MLearningController:
     def __init__(self):
         self.mlearning_service = MLearningService()
 
-    def knn(self, entrenamiento):
-        return self.mlearning_service.knn(entrenamiento)
+    def knn(self, entrenamiento: InfoEntrenamiento):
+        if entrenamiento.nombre_algoritmo.upper() == "KNN":
+            return self.mlearning_service.knn(entrenamiento)
+        elif entrenamiento.nombre_algoritmo.upper() == "NAIVE BAYES":
+            pass
+        elif entrenamiento.nombre_algoritmo.upper() == "REGRESION LOGISTICA":
+            pass
+        elif entrenamiento.nombre_algoritmo.upper() == "SVM":
+            pass
+        elif entrenamiento.nombre_algoritmo.upper() == "ARBOL DE DECISION":
+            pass
+        elif entrenamiento.nombre_algoritmo.upper() == "REGRESION LOGISTICA":
+            pass
+        else:
+            return "Algoritmo no encontrado"
+
     
     def reg_logistica(self):
         #return self.mlearning_service.regresion_logistica()
