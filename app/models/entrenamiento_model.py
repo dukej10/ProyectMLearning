d4ad725978735dde1ae6e10c1ecd3cb44e4b0944
JDuque <diegoduqqe@gmail.com>
2023-06-03 01:29:11 -0500

    información de entrenamiento

---
+++ app/models/entrenamiento_model.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+from typing import List
+
+class InfoEntrenamiento(BaseModel):
+    nombre_algoritmo: str
+    columnas_x: List[str]
+    objetivo_y: str
+    tecnica: str
+    cantidad: int
+    normalizacion: str
