7c500199fe6ca7c74664efb589853d9cad42a720
JDuque <diegoduqqe@gmail.com>
2023-06-04 20:48:55 -0500

    actualizar o guardar métricas del modelo

--- app/services/mongodb_service.py
+++ app/services/mongodb_service.py
@@ -18,6 +18,39 @@ class MongoDBService:
             print(f'Error al guardar el JSON en MongoDB: {str(e)}')
             return None
         
+    def guardar_json_metricas(self, datos_json, coleccion):
+        try:
+            self.collection = self.db[coleccion]
+            
+            # Verificar si existe un registro con el nombre del algoritmo, normalización y técnica
+            existe_registro = self.collection.find_one({
+                "nombre_algoritmo": datos_json['nombre_algoritmo'],
+                "normalizacion": datos_json['normalizacion'],
+                "tecnica": datos_json['tecnica']
+            })
+            
+            if existe_registro:
+                # Actualizar los datos del registro existente
+                result = self.collection.update_one(
+                    {
+                        "nombre_algoritmo": datos_json['nombre_algoritmo'],
+                        "normalizacion": datos_json['normalizacion'],
+                        "tecnica": datos_json['tecnica']
+                    },
+                    {"$set": datos_json}
+                )
+                print(f'Datos actualizados en MongoDB: {str(result)}')
+                return result.upserted_id
+            else:
+                # Insertar un nuevo registro
+                result = self.collection.insert_one(datos_json)
+                print(f'JSON guardado en MongoDB: {str(result)}')
+                return result.inserted_id
+        except Exception as e:
+            print(f'Error al guardar o actualizar el JSON en MongoDB: {str(e)}')
+            return None
+    
+
     def obtener_ultimo_registro(self, coleccion):
         try:
             self.collection = self.db[coleccion]
